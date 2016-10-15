538e6f52f1591d07a80a62dc134148f276b639f1
maurosilvarojas <reggaecol@gmail.com>
2016-10-15 13:47:15 +0100

    Initial commit

---
+++ src/helloWorld.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+//lab1 Write a simple program in Java using Eclipse IDE to print the greeting “Hello World” to the console.
+public class helloWorld {
+    public static void main(String[] args) {
+       check(3);
+
+    }
+
+    private static void check(int z) {
+        switch (z){
+            case 1 : z=1;
+                exe1();
+                break;
+            case 2 : z=2;
+                exe2();
+                break;
+            case 3 : z=3;
+                exe2();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public static void exe2() {
+        System.out.println("knock/* /n knokkkkk");
+    }
+
+    //System.out.println("A quoted String is much better  if you learn the rules of escape sequences");
+       /* System.out.println(" ");
+        System.out.println("Also/, /"/" represent an empty string");
+        System.out.println("");
+        System.out.println("Don’t forget to use /” to print /“/” on your console /n Note that there are 2 blank lines in this output.");*/
+
+    public static void exe1() {
+        System.out.println("hello World");//first
+
+    }
+}
